6b02415481f6b0b3fdf61eaf15b8af37dbaa0033
narwhalblast <ognomisch@gmail.com>
2021-03-13 17:52:22 -0500

    Latest Update
    aoeua

--- src/zebra/Game.java
+++ src/zebra/Game.java
@@ -11,22 +11,17 @@ public class Game {
 	public Game() {
 		gameObjects = new ArrayList<GameObject>(0);
 		
-		player = new GameObject(new Vector(600, 600), 25, 25, Color.red, new Physics(1));
+		player = Instantiate(new GameObject(new Vector(600, 600), 25, 25, Color.red, new Physics(1)));
 		player.physics.velocity.x = 0.2;
-		GameObject platform = new GameObject(new Vector(645, 500), 25, 25, Color.pink, new Physics(1));
-		platform.physics.velocity.x = 0.1;
-		
-		GameObject platform2 = new GameObject(new Vector(640, 100), 640, 24, Color.pink);
-		GameObject thing = new GameObject(new Vector(-100, 600), 25, 25, Color.red, new Physics(1));
+		GameObject box = Instantiate(new GameObject(new Vector(645, 500), 25, 25, Color.pink, new Physics(1)));
+		box.physics.velocity.x = 0.1;
+		GameObject thing = Instantiate(new GameObject(new Vector(-100, 600), 25, 25, Color.red, new Physics(1)));
 		thing.physics.velocity.x = 1;
 		
-		GameObject platform3 = new GameObject(new Vector(997, 250), 24, 320, Color.pink);
-		
-		gameObjects.add(player);
-		gameObjects.add(platform);
-		gameObjects.add(platform2);
-		gameObjects.add(platform3);
-		gameObjects.add(thing);
+		Instantiate(new GameObject(new Vector(997, 250), 24, 320, Color.pink));
+		Instantiate(new GameObject(new Vector(640, 100), 640, 24, Color.pink));
+		Instantiate(new GameObject(new Vector(396, 124), 150, 24, Color.pink));
+
 	}
 	
 	void drawToScreen(Screen screen) {
